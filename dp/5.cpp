bf7a64b8d5e92b241fe54ad301f303f26387f0d4
noogler-eng <sharadpoddar10102003@gmail.com>
2025-01-13 15:33:27 +0530

    dp

--- dp/5.cpp
+++ dp/5.cpp
@@ -29,3 +29,7 @@ class Solution {
         return len;	
     }
 };
+
+
+// memorization method
+
---
+++ recurssion/1.cpp
@@ -0,0 +1,30 @@
+// All subsets
+// subsets contains all substring and sunarray
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void findSubsets(vector<int> &nums, vector<int> &current, int index, vector<vector<int>> &result) {
+    if (index == nums.size()) {
+        result.push_back(current);
+        return;
+    }
+
+    // not selecting
+    findSubsets(nums, current, index + 1, result);
+
+    // selecting
+    current.push_back(nums[index]);
+    findSubsets(nums, current, index + 1, result);
+    current.pop_back();
+}
+
+vector<vector<int>> subsets(vector<int> &nums) {
+    vector<vector<int>> result;
+    vector<int> current;
+    findSubsets(nums, current, 0, result);
+    return result;
+}
+
